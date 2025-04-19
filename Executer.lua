81edf6907e45de27cc649e1ae207073ec141ffb3
Deniz <csgcreepysmilegroup@gmail.com>
2025-04-19 23:27:00 +0300

    Update Executer.lua

--- Executer.lua
+++ Executer.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/ezpoSword/Rayfield_Roblox/refs/heads/main/RayfieldMenuCode-No-tWorking'))()
 
 
 
